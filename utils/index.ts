0e9116976c0539ee02044acffd34663b0e36e200
Slawomir Dyk <slawomir.dyk@astrosat.net>
2023-12-02 20:01:16 +0000

    Implemented carCard and carDetails components, integrated with Imagin Studio API to fetch car images.

--- utils/index.ts
+++ utils/index.ts
@@ -1,3 +1,5 @@
+import { CarProps } from "../types";
+
 export async function fetchCars() {
   const headers = {
     "X-RapidAPI-Key": "b52e23b949msh78e586cd5ed117ap10af61jsnbd585ff7d7ca",
@@ -17,3 +19,36 @@ export async function fetchCars() {
 
   return result;
 }
+
+export const calculateCarRent = (city_mpg: number, year: number) => {
+  const basePricePerDay = 50; // Base rental price per day in dollars
+  const mileageFactor = 0.1; // Additional rate per mile driven
+  const ageFactor = 0.05; // Additional rate per year of vehicle age
+
+  // Calculate additional rate based on mileage and age
+  const mileageRate = city_mpg * mileageFactor;
+  const ageRate = (new Date().getFullYear() - year) * ageFactor;
+
+  // Calculate total rental rate per day
+  const rentalRatePerDay = basePricePerDay + mileageRate + ageRate;
+
+  return rentalRatePerDay.toFixed(0);
+};
+
+export const getCarImageUrl = (car: CarProps, angle?: string) => {
+  const url = new URL("https://cdn.imagin.studio/getimage");
+  const { make, model, year } = car;
+
+  url.searchParams.append(
+    "customer",
+    "hrjavascript-mastery"
+  );
+  url.searchParams.append("make", make);
+  url.searchParams.append("modelFamily", model.split(" ")[0]);
+  url.searchParams.append("zoomType", "fullscreen");
+  url.searchParams.append("modelYear", `${year}`);
+  // url.searchParams.append('zoomLevel', zoomLevel);
+  url.searchParams.append("angle", `${angle}`);
+
+  return `${url}`;
+};
